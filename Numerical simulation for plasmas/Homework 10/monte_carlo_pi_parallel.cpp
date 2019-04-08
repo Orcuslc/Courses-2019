088c715d730f9fc50cada87367e41365687726c0
Chuan <chuanlu@argon-login-1.hpc>
2019-04-08 15:33:50 -0500

    update

--- Numerical simulation for plasmas/Homework 10/monte_carlo_pi_parallel.cpp
+++ Numerical simulation for plasmas/Homework 10/monte_carlo_pi_parallel.cpp
@@ -1,8 +1,11 @@
 #include "mpi.h"
+#include <cstdlib>
 #include <iostream>
+#include <time.h>
 using namespace std;
 
-int N = 1e8;
+// int N = 1e8;
+
 
 double random(double MIN, double MAX) {
     double r = (double)rand() / RAND_MAX;
@@ -12,22 +15,23 @@ double random(double MIN, double MAX) {
 inline bool in_circle(double x, double y) {
     return x*x + y*y <= 1.0;
 }
-
+/*
 void get_input(int argc, char* argv[], int rank, int* per_processor) {
     // for root node
     if(rank == 0) {
         int N_processor = atoi(argv[1]);
         *per_processor = N/(N_processor);
-        cout << *per_processor << endl;
     }
-
     
     // broadcast per_processor from root to other nodes
     MPI_Bcast(per_processor, 1, MPI_INT, 0, MPI_COMM_WORLD);
 }
+*/
 
 int main(int argc, char** argv) {
-    int count = 0, per_processor;
+    int N = 1e8;
+    int count = 0, per_processor, total_count;
+    double start, cost, elapsed;
     
     // init
     MPI_Init(&argc, &argv);
@@ -35,43 +39,42 @@ int main(int argc, char** argv) {
     // get number of processes
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    
+
+    per_processor = N/world_size;
+    N = per_processor*world_size;
+
     // get rank of process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
     // get input information
-    get_input(argc, argv, world_rank, &per_processor);
+ //   get_input(argc, argv, world_rank, &per_processor);
 
     srand(time(NULL)+world_rank);
 
-    // the node with world rank == 0 is regarded as the master node
-    if(world_rank != 0) {
-        for(int i = 0; i < per_processor; i++) { 
-
-            if(i % 1000000 == 0) {
-                cout << world_rank << endl;
-                cout << i << endl;
-            }
-            double x = random(-1., 1.);
-            double y = random(-1., 1.);
-            if(in_circle(x, y))
-                count++;
-        }
+    // timed
+    MPI_Barrier(MPI_COMM_WORLD);
+    start = MPI_Wtime();
+    for(int i = 0; i < per_processor; i++) { 
+        double x = random(-1., 1.);
+        double y = random(-1., 1.);
+        if(in_circle(x, y))
+            count++;
     }
-    
-    // for master node
-    int total_count, total_N;
+    cost = MPI_Wtime() - start;
 
+    // reduce time 
+    MPI_Reduce(&cost, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    
     // reduce the computed result to root node
     MPI_Reduce(&count, &total_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&per_processor, &total_N, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    cout << total_count << " " << total_N << " " << (double)total_count/(double)total_N << endl;
     // operation at root node
     if(world_rank == 0) {
-        double res = (double)total_count/(double)total_N*4.0;
+        double res = (double)total_count/(double)N*4.0;
+        cout.precision(17);
         cout << res << endl;
+        cout << elapsed << endl;
     }
 
     MPI_Finalize();
