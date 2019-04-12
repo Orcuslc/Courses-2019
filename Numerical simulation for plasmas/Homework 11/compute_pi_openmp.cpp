0e838d183a06cf6f13eb09482094a0a89b173f28
Chuan <orcuslc@hotmail.com>
2019-04-11 23:49:59 -0500

    update HW 11

--- Numerical simulation for plasmas/Homework 11/compute_pi_openmp.cpp
+++ Numerical simulation for plasmas/Homework 11/compute_pi_openmp.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(int argc, char* argv[]) {
     int total_threads;
     double pi, sum;
+    int N = 1e9;
 
-#pragma omp parallel private()
+    #pragma omp parallel for reduction (+:sum)
+    for(int i = 0; i < N; i++) {
+        sum += pow(-1, i)/(2.*i+1.);
+    }
+
+    #pragma omp master
+    {
+        pi = 4*sum;
+        cout.precision(17);
+        cout << pi << endl;
+    }
+    return 0;
 }
